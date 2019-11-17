064967bb9ae719dad8e36ac462b0495fbf081a59
harshiththota <thota.harshit@gmail.com>
2019-11-17 17:56:40 +0530

    Command line functions

---
+++ index.js
@@ -0,0 +1,30 @@
+const Helper = require('./helper');
+
+const RELATION_TYPE = {
+  SYNONYM: 'synonym',
+  ANTONYM: 'antonym',
+};
+
+const func = process.argv[2];
+const word = process.argv[3];
+
+switch(func) {
+  case 'defn':
+    return Helper.definitions(word);
+  
+  case 'syn':
+    return Helper.relatedWords(word, RELATION_TYPE.SYNONYM);
+
+  case 'ant':
+    return Helper.relatedWords(word, RELATION_TYPE.ANTONYM);
+
+  case 'ex':
+    return Helper.example(word);
+
+  default:
+    if (func) {
+      return Helper.fullDist(func);
+    } else {
+      return Helper.dayFullDist();
+    }
+};
